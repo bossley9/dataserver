4b16dd58b44801164dafec6576265465e3245fbc
Sam Bossley <bossley.samuel@gmail.com>
2023-04-27 00:57:28 -0700

    chore: add clarifying comment to add admin gitea user

--- services/gitea.nix
+++ services/gitea.nix
@@ -9,6 +9,7 @@
       repository = {
         DEFAULT_PRIVATE = "private";
       };
+      # temporarily set to false to register an admin user
       service.DISABLE_REGISTRATION = true;
       server = {
         HTTP_PORT = 8003;
