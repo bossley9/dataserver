e71d8b672f212cb3c362dd409b2e31af45d68a7e
Sam Bossley <bossley.samuel@gmail.com>
2023-04-27 00:28:15 -0700

    feat: migrate nextcloud instance from sqlite to postgres

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -9,9 +9,28 @@
       adminuser = "admin";
       adminpassFile = "${pkgs.writeText "nextcloud-initial-credentials" "test1234!"}";
       defaultPhoneRegion = "US";
+      dbhost = "/run/postgresql";
+      dbname = "nextcloud";
+      dbtype = "pgsql";
+      dbuser = "nextcloud";
     };
     https = true;
     maxUploadSize = "2G";
     enableImagemagick = false; # see https://github.com/nextcloud/server/issues/13099
+  };
+  # ensure Postgres DB setup happens before Nextcloud
+  systemd.services."nextcloud-setup" = {
+    requires = [ "postgresql.service" ];
+    after = [ "postgresql.service" ];
+  };
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "nextcloud" ];
+    ensureUsers = [
+      {
+        name = "nextcloud";
+        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
+      }
+    ];
   };
 }
